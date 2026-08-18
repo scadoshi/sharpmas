191dd1faa621703425d872db27f7cd4fc0fcd7dc
scadoshi <scottyfermo@hotmail.com>
2026-08-18 14:53:18 -0700

    Remove solution files that were swept onto main by mistake

--- src/Sharpmas/Domain/Solution/Common/Point.cs
+++
@@ -1,7 +0,0 @@
-namespace Sharpmas.Domain.Solution.Common;
-
-public class Point
-{
-    public required int X { get; set; }
-    public required int Y { get; set; }
-}
--- src/Sharpmas/Domain/Solution/Year2016/Day01/Instruction.cs
+++
@@ -1,9 +0,0 @@
-using Sharpmas.Domain.Solution.Common;
-
-namespace Sharpmas.Domain.Solution.Year2016;
-
-public class Instruction
-{
-    public Turn Turn { get; set; }
-    public int Distnace { get; set; }
-}
